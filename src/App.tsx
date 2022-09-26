7327588be0bcc8ed17d2d0a7dc248c7bf1db7c7c
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2022-09-26 18:54:03 -0300

    :sparkles: feat: loading photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
-import {Area, Container, Header} from './App.styles'
+import { useState, useEffect } from 'react'
+import {Area, Container, Header, Loading, PhotoList} from './App.styles'
+import * as Photos from './services/photos'
+import { Photo } from './types/Photo'
 
 function App() {
+    const [loading, setLoading] = useState(false)
+    const [photos, setPhotos] = useState<Photo[]>([])
+
+    useEffect(() =>{
+        const getPhotos = async () =>{
+            setLoading(true)
+            setPhotos(await Photos.getAll())
+            setLoading(false)
+        }
+        getPhotos()
+    },[])
+
     return (
         <Container>
             <Area>
                 <Header>Galeria de Fotos</Header>
+                {loading && 
+                    <Loading>
+                        <div className='emoji'>✋</div>
+                        <div>Carregando...</div>
+                    </Loading>
+                }
+                {!loading && photos.length > 0 &&
+                    <PhotoList>
+                        {photos.map((item, index)=>(
+                            <div>{item.name}</div>
+                        ))}
+                    </PhotoList>    
+                }
+                {!loading && photos.length === 0 &&
+                    <Loading>
+                        <div className='emoji'>💔</div>
+                        <div>Não há fotos...</div>
+                    </Loading>    
+                }
             </Area>
         </Container>
     )
